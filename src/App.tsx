f8e8e29fdcc20bc4aa242d718c958ac742aea1e7
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-11 23:30:17 +0200

    chore: added map

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './index.css'
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import {useQuery} from '@tanstack/react-query';
 import {getPostcodeDetails, getPostcodes} from "./api/postcodes.ts";
 import {TextField} from './components/TextField'
 import {Title} from './components/Title'
 import {useDebounce} from './hooks/useDebounce'
 import {Table} from "./components/Table.tsx";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map, { MapRef, Marker } from 'react-map-gl';
 
 function App() {
 
@@ -13,6 +15,7 @@ function App() {
     const debouncedInput = useDebounce(input, 800);
     const [activePostcode, setActivePostcode] = useState(null);
     const [dropdownVisible, setDropdownVisible] = useState(false);
+    const mapRef = useRef<MapRef>(null);
 
     const {data, isFetching} = useQuery({
         queryKey: ['postcodes', debouncedInput],
@@ -45,26 +48,43 @@ function App() {
     };
 
     return (
-        <div className="flex flex-col justify-start items-start">
-            <Title/>
-            <div className="my-4 w-full">
-                <TextField
-                    placeholder="Enter postcode..."
-                    value={input}
-                    onChange={onChangeInputText}
-                    suggestions={dropdownVisible ? data?.result : []}
-                    onSuggestionClick={handleSuggestionClick}
-                />
+            <div className="grid grid-cols-1 sm:grid-cols-2 h-full">
+                <div className="h-full">
+                    <Title/>
+                    <div className="my-4 w-full">
+                        <TextField
+                            placeholder="Enter postcode..."
+                            value={input}
+                            onChange={onChangeInputText}
+                            suggestions={dropdownVisible ? data?.result : []}
+                            onSuggestionClick={handleSuggestionClick}
+                        />
+                    </div>
+                    {postcodeDetails && (
+                        <Table data={postcodeDetails.result} />
+                    )}
+                </div>
+
+                <div className="h-full">
+                    <Map
+                        initialViewState={{
+                            latitude: 53.27515756,
+                            longitude: -7.877257,
+                            zoom: 6.5,
+                            bearing: 0,
+                            pitch: 0,
+                        }}
+                        ref={mapRef}
+                        style={{ width: '100%', height: '100vh' }}
+                        mapboxAccessToken="pk.eyJ1IjoiYWxlcGhyaSIsImEiOiJjamdwbHpycjIyZm45Mndud3AzamRibHpqIn0.ejAHwSGT6dcGxiDOrPCFLg"
+                        mapStyle="mapbox://styles/mapbox/streets-v12"
+                        dragRotate={false}
+                        touchZoomRotate={false}
+                        attributionControl={false}
+                    >
+                    </Map>
+                </div>
             </div>
-            {/*{isFetching && <p>Loading...</p>}*/}
-            {postcodeDetails && (
-                <Table data={postcodeDetails.result} />
-                // <div>
-                //     <p>Details for: {postcodeDetails.result.postcode}</p>
-                //     <p>Region: {postcodeDetails.result.european_electoral_region}</p>
-                // </div>
-            )}
-        </div>
     );
 }
 
